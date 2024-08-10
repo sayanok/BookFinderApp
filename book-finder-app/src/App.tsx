bab3ada528d735d313feb509ec00e4316b081c08
bigen1925 <bigen1925@gmail.com>
2024-08-10 16:22:18 +0900

    検索結果が表示されるようにした

--- book-finder-app/src/App.tsx
+++ book-finder-app/src/App.tsx
@@ -1,19 +1,40 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 const App: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
+  const [resultList, setResultList] = useState<Array<any>>([]);
   const [result, setResult] = useState<string>("");
 
   function search() {
+    console.log(inputValue);
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + inputValue).then(function (response) {
+      // handle success
+      setResultList(response.data.items);
+    });
     setResult("検索結果");
   }
 
   return (
     <>
-      <input placeholder={"本のタイトルや著者を入力してください"} size={50}></input>
+      <input
+        placeholder={"本のタイトルや著者を入力してください"}
+        size={50}
+        onChange={(e) => setInputValue(e.target.value)}
+      ></input>
       <button onClick={() => search()}>検索する</button>
       <div>結果を表示する</div>
       <p>{result}</p>
+      <div>
+        {resultList.map((data) => (
+          <>
+            <p>タイトル：{data.volumeInfo.title}</p>
+            <p>著者：{data.volumeInfo.authors}</p>
+            <p>発行日：{data.volumeInfo.publishedDate}</p>
+            <img src={data.volumeInfo.imageLinks.smallThumbnail} />
+          </>
+        ))}
+      </div>
     </>
   );
 };
